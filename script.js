62ea5a7b73f6b2dc1e992d3685f06b541ae9a537
kareiku <xaxvu7@gmail.com>
2023-11-03 22:21:58 +0100

    Major changes everywhere.

--- script.js
+++ script.js
@@ -1,7 +1,570 @@
-const buttons = ["My Library", "Piano", "Wii Softmoding", "Astronomy Picture of the Day Archive", "Vimm's Vault", "Nintendo Roms", "Steven Universe Archive"];
-const links = ["library/index.html", "piano/index.html", "https://wii.guide", "https://apod.nasa.gov/apod/astropix.html", "https://vimm.net/vault", "https://r-roms.gitlab.io/megathread/sections/nintendo", "http://nekit.sytes.net/files/gems"];
+const library = [
+    {
+        name: "Assassination Classroom",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/assassination_classroom.png"
+    },
+    {
+        name: "Sword Art Online",
+        type: "Anime Series",
+        status: "Watched Seasons 1-2",
+        image: "./assets/images/sao.png"
+    },
+    {
+        name: "No Game No Life",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/ngnl.png"
+    },
+    {
+        name: "Death Note",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/death_note.png"
+    },
+    {
+        name: "Shingeki no Kyojin",
+        type: "Anime Series",
+        status: "Last Season Remaining",
+        image: "./assets/images/snk.png"
+    },
+    {
+        name: "Re:Zero",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/re-zero.png"
+    },
+    {
+        name: "Blue Exorcist",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/blue_exorcist.png"
+    },
+    {
+        name: "Fairy Tail",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/fairy_tail.png"
+    },
+    {
+        name: "Little Witch Academia",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/lwa.png"
+    },
+    {
+        name: "Your Lie in April",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/your_lie_in_april.png"
+    },
+    {
+        name: "Black Clover",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/black_clover.png"
+    },
+    {
+        name: "K-ON!",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/k-on.png"
+    },
+    {
+        name: "Haikyuu!!",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/haikyuu.png"
+    },
+    {
+        name: "Hibike Euphonium",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/hibike_euphonium.png"
+    },
+    {
+        name: "Kakegurui",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/kakegurui.png"
+    },
+    {
+        name: "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/sbywbgsnywm.png"
+    },
+    {
+        name: "Jujutsu Kaisen",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/jujutsu_kaisen.png"
+    },
+    {
+        name: "Sora yori mo Tooi Basho",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/sora_yori.png"
+    },
+    {
+        name: "Kobayashi-san Chi no Maid Dragon",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/kobayashi-san.png"
+    },
+    {
+        name: "Jojo's Bizarre Adventure",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/jjba.png"
+    },
+    {
+        name: "Wonder Egg Priority",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/wep.png"
+    },
+    {
+        name: "Anohana",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/anohana.png"
+    },
+    {
+        name: "Fate/Apocrypha",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/fat-apocrypha.png"
+    },
+    {
+        name: "Yuru Camp",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/yuru_camp.png"
+    },
+    {
+        name: "Majo no Tabitabi",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/majo_no_tabitabi.png"
+    },
+    {
+        name: "Naruto",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/naruto.png"
+    },
+    {
+        name: "Adachi to Shimamura",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/adachi_to_shimamura_(anime).png"
+    },
+    {
+        name: "Adachi to Shimamura",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/adachi_to_shimamura_(manga).png"
+    },
+    {
+        name: "Komi-san wa, Komyushou Desu",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/komi-san.png"
+    },
+    {
+        name: "Menhera Shoujo Kurumi-chan",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/kurumi-chan.png"
+    },
+    {
+        name: "Bloom Into You",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/bloom_into_you.png"
+    },
+    {
+        name: "Science Fell in Love, So I Tried to Prove It",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Spy x Family",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/sxf.png"
+    },
+    {
+        name: "Jibaku Shōnen Hanako-kun",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Link Click",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Classroom of the Elite",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Non Non Biyori",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Hōseki no Kuni",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "RWBY",
+        type: "Animated Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "RWBY: Ice Queendom",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "RWBY Anthology",
+        type: "Manga",
+        status: "Read the first volume",
+        image: "./assets/images/"
+    },
+    {
+        name: "Lycoris Recoil",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Oregairu",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Bocchi the Rock!",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Beyond the Boundary",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Mobile Suit Gundam: The Witch from Mercury",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Domestic na Kanojo",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Kimi no Na wa.",
+        type: "Anime Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Kimi no Na wa.",
+        type: "Manga",
+        status: "Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Tenki no Ko",
+        type: "Anime Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Kimi no Suizō o Tabetai",
+        type: "Anime Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Bubble",
+        type: "Anime Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Kase-san Shirīzu",
+        type: "Anime Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Asagao to Kase-san / Yamada to Kase-san",
+        type: "Manga",
+        status: "Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Suzume no Tojimari",
+        type: "Anime Movie",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "A Silent Voice",
+        type: "Anime Movie",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Arcane",
+        type: "Animated Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Steven Universe",
+        type: "Animated Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Persona 5: The Animation",
+        type: "Anime Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Persona 5",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Stranger Things",
+        type: "Series",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Adam by Eve: A Live in Animation",
+        type: "Live Action Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "The Super Mario Bros. Movie",
+        type: "Animated Movie",
+        status: "Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Alter Ego",
+        type: "Manga",
+        status: "Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Noel y June",
+        type: "Manga",
+        status: "Not yet released",
+        image: "./assets/images/"
+    },
+    {
+        name: "Sirius",
+        type: "Manga",
+        status: "Read first chapter",
+        image: "./assets/images/"
+    },
+    {
+        name: "Déjame que te cuente...",
+        type: "Book",
+        status: "I think I finished it",
+        image: "./assets/images/"
+    },
+    {
+        name: "Mirai Nikki",
+        type: "Anime Movie",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Another",
+        type: "Anime Movie",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Ranking of Kings",
+        type: "Anime Movie",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Breaking Bad",
+        type: "Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Chilling Adventures of Sabrina",
+        type: "Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "The Owl House",
+        type: "Animated Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "You",
+        type: "Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Cunk on Earth",
+        type: "Comedy Documentary",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Cyberpunk: Edgerunners",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Given",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Sasaki to Miyano",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "I'm in Love with the Villainess",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Whisper Me a Love Song",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "I Married My Best Friend To Shut My Parents Up",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Lonely Girl ni Sakaraenai",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Alcohol Yuri Anthology: Strong!",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Throw Away the Suit Together",
+        type: "Manga",
+        status: "Not Read",
+        image: "./assets/images/"
+    },
+    {
+        name: "Hyouka",
+        type: "Anime Series",
+        status: "Not Seen",
+        image: "./assets/images/"
+    },
+    {
+        name: "Why is my crush so hard on me?",
+        type: "Doujin Manga",
+        status: "Read",
+        image: "./assets/images/"
+    }
+];
 
-buttons.forEach((button, index) => {
-    const link = links[index];
-    document.write(`<input type="button" value="${button}" onclick='location.href="${link}"'><br>`);
+const links = [
+    {
+        label: "Piano",
+        URL: "./piano/index.html"
+    },
+    {
+        label: "Steven Universe Archive",
+        URL: "http://nekit.sytes.net/files/gems"
+    },
+    {
+        label: "Astronomy Picture of the Day",
+        URL: "https://apod.nasa.gov/apod/astropix.html"
+    },
+    {
+        label: "Wii Softmodding Guide",
+        URL: "https://wii.guide"
+    },
+    {
+        label: "Vimm's Vault",
+        URL: "https://vimm.net/vault"
+    },
+    {
+        label: "Nintendo Roms",
+        URL: "https://r-roms.gitlab.io/megathread/sections/nintendo"
+    }
+];
+
+document.write('<div class="links-container">');
+document.write('<div class="links-wrapper">');
+document.write('<div id="links-container">');
+links.forEach(function (link) {
+    document.write(`
+        <div class="link-item">
+          <input type="button" value="${link.label}" onclick='location.href="${link.URL}"' class="link">
+        </div>
+    `);
+});
+document.write('</div>');
+document.write('</div>');
+document.write('</div>');
+
+library.forEach(function (item) {
+    document.write(`
+        <div class="library-container">
+          <img class="library-image" src="${item.image}">
+          <div class="library-info">
+            - ${item.name}<br>- ${item.type}<br>- ${item.status}
+          </div>
+        </div>
+    `);
 });
