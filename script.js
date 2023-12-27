6d41cb3fe54b5589173088a8b5f2ea9c694bdd4c
kareiku <xaxvu7@gmail.com>
2023-12-27 17:06:00 +0100

    .

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ document.write('</div>');
 
 document.write('<div>');
 addItem("library", "Assassination Classroom", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/5/75639l.jpg");
-addItem("library", "Sword Art Online", "Anime Series", "Seen Seasons 1-2", "https://cdn.myanimelist.net/images/anime/8/36343l.jpg");
+addItem("library", "Sword Art Online", "Anime Series", "Dropped after S2", "https://cdn.myanimelist.net/images/anime/8/36343l.jpg");
 addItem("library", "No Game No Life", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1074/111944l.jpg");
 addItem("library", "Death Note", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1001/121564l.jpg");
-addItem("library", "Shingeki no Kyojin", "Anime Series", "Last Season Remains", "https://cdn.myanimelist.net/images/anime/1907/134102l.jpg");
+addItem("library", "Shingeki no Kyojin", "Anime Series", "Last Season Remaining", "https://cdn.myanimelist.net/images/anime/1907/134102l.jpg");
 addItem("library", "Re:Zero", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1534/134863l.jpg");
 addItem("library", "Blue Exorcist", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/10/75195l.jpg");
 addItem("library", "Fairy Tail", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/7/59441l.jpg");
@@ -63,11 +63,12 @@ addItem("library", "Little Witch Academia", "Manga", "Unread", "https://cdn.myan
 addItem("library", "Your Lie in April", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/3/72831l.jpg");
 addItem("library", "Black Clover", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1232/93334l.jpg");
 addItem("library", "K-On!", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/10/76120l.jpg");
+addItem("library", "K-On!", "Manga", "Unread", "https://static.bunnycdn.ru/i/cache/images/7/77/77201fbbedf0f8624a956fe824d5fe39.jpg");
 addItem("library", "Haikyuu!!", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/7/76014l.jpg");
 addItem("library", "Hibike Euphonium", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/6/72445l.jpg");
 addItem("library", "Kakegurui", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/3/86578l.jpg");
 addItem("library", "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1895/119503l.jpg");
-addItem("library", "Jujutsu Kaisen", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1171/109222l.jpg");
+addItem("library", "Jujutsu Kaisen", "Anime Series", "Watching", "https://cdn.myanimelist.net/images/anime/1171/109222l.jpg");
 addItem("library", "Sora yori mo Tooi Basho", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/12/89878l.jpg");
 addItem("library", "Kobayashi-san Chi no Maid Dragon", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/5/85434l.jpg");
 addItem("library", "Jojo's Bizarre Adventure", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/3/40409l.jpg");
@@ -80,7 +81,7 @@ addItem("library", "Adachi to Shimamura", "Anime Series", "Unseen", "https://cdn
 addItem("library", "Adachi to Shimamura", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/2/232193l.jpg");
 addItem("library", "Komi-san wa, Komyushou Desu", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1899/117237l.jpg");
 addItem("library", "Menhera Shoujo Kurumi-chan", "Manga", "Unread", "https://static.bunnycdn.ru/i/cache/images/9/9d/9d3a6c50621ea7c8736ed04ee26df849.jpg");
-addItem("library", "Bloom Into You", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/1/232311l.jpg");
+addItem("library", "Bloom Into You", "Manga", "Reading", "https://cdn.myanimelist.net/images/manga/1/232311l.jpg");
 addItem("library", "Bloom Into You Antology", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/2/222782l.jpg");
 addItem("library", "Bloom Into You", "Anime", "Unseen", "https://cdn.myanimelist.net/images/anime/1783/96153l.jpg");
 addItem("library", "Science Fell in Love, So I Tried to Prove It", "Anime Series", "Seen", "https://cdn.myanimelist.net/images/anime/1432/103533l.jpg");
@@ -92,7 +93,7 @@ addItem("library", "Non Non Biyori", "Anime Series", "Unseen", "https://cdn.myan
 addItem("library", "Hōseki no Kuni", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/3/88293l.jpg");
 addItem("library", "RWBY", "Animated Series", "Seen", "https://pics.filmaffinity.com/rwby-751014904-large.jpg");
 addItem("library", "RWBY: Ice Queendom", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1356/121832l.jpg");
-addItem("library", "RWBY Anthology", "Manga", "First volume read", "https://cdn.myanimelist.net/images/manga/2/197717l.jpg");
+addItem("library", "RWBY Anthology", "Manga", "V2-4 remaining", "https://cdn.myanimelist.net/images/manga/2/197717l.jpg");
 addItem("library", "Lycoris Recoil", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1392/124401l.jpg");
 addItem("library", "Oregairu", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1786/120117l.jpg");
 addItem("library", "Bocchi the Rock!", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1448/127956l.jpg");
@@ -116,9 +117,9 @@ addItem("library", "Adam by Eve: A Live in Animation", "Live Action Movie", "See
 addItem("library", "The Super Mario Bros. Movie", "Animated Movie", "Seen", "https://pics.filmaffinity.com/the_super_mario_bros_movie-521125124-large.jpg");
 addItem("library", "Alter Ego", "Manga", "Read", "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/ac114741-9438-4561-a749-746ebf448762/d_1200_1200/portada_planeta-manga-alter-ego__202011171333.webp");
 addItem("library", "Noel y June", "Manga", "Unreleased", "https://pbs.twimg.com/media/Fj9mC9BXgAA2_3U?format=jpg&name=4096x4096");
-addItem("library", "Sirius", "Manga", "Read first chapter", "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/32aa5e1c-6d4c-498a-b0f9-cf0340fc8d85/d_360_620/portada_planeta-manga-sirius_ana-c-sanchez_202201121309.webp");
-addItem("library", "Limbo","Manga","Unread","https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/4403a9cf-a87f-4605-a57f-3aca85955713/d_1200_1200/portada_planeta-manga-limbo-n-01__202212220954.webp")
-addItem("library", "Déjame que te cuente...", "Book", "I think I finished it", "https://m.media-amazon.com/images/I/81mvurKh0YL._AC_UF1000,1000_QL80_.jpg");
+addItem("library", "Sirius", "Manga", "C2-... remaning", "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/32aa5e1c-6d4c-498a-b0f9-cf0340fc8d85/d_360_620/portada_planeta-manga-sirius_ana-c-sanchez_202201121309.webp");
+addItem("library", "Limbo", "Manga", "Unread", "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/4403a9cf-a87f-4605-a57f-3aca85955713/d_1200_1200/portada_planeta-manga-limbo-n-01__202212220954.webp")
+addItem("library", "Déjame que te cuente...", "Book", "Read", "https://m.media-amazon.com/images/I/81mvurKh0YL._AC_UF1000,1000_QL80_.jpg");
 addItem("library", "Mirai Nikki", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1059/128279l.jpg");
 addItem("library", "Another", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/4/75509l.jpg");
 addItem("library", "Ranking of Kings", "Anime Series", "Unseen", "https://cdn.myanimelist.net/images/anime/1347/117616l.jpg");
@@ -131,9 +132,9 @@ addItem("library", "Cyberpunk: Edgerunners", "Anime Series", "Unseen", "https://
 addItem("library", "Given", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/1/172497l.jpg");
 addItem("library", "Given", "Anime", "Unseen", "https://cdn.myanimelist.net/images/anime/1666/102238l.jpg");
 addItem("library", "Sasaki to Miyano", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/3/187122l.jpg");
-addItem("library", "I'm in Love with the Villainess", "Manga", "Started", "https://cdn.myanimelist.net/images/manga/2/238641l.jpg");
-addItem("library", "I'm in Love with the Villainess", "Anime Series", "Started", "https://cdn.myanimelist.net/images/anime/1531/137711l.jpg");
-addItem("library", "Whisper Me a Love Song", "Manga", "Read and waitin' for more~", "https://cdn.myanimelist.net/images/manga/1/219489l.jpg");
+addItem("library", "I'm in Love with the Villainess", "Manga", "Waiting for new releases", "https://cdn.myanimelist.net/images/manga/2/238641l.jpg");
+addItem("library", "I'm in Love with the Villainess", "Anime Series", "Waiting for new releases", "https://cdn.myanimelist.net/images/anime/1531/137711l.jpg");
+addItem("library", "Whisper Me a Love Song", "Manga", "Waiting for new releases", "https://cdn.myanimelist.net/images/manga/1/219489l.jpg");
 addItem("library", "Whisper Me a Love Song", "Anime", "Unreleased", "https://cdn.myanimelist.net/images/anime/1518/137892l.jpg");
 addItem("library", "I Married My Best Friend To Shut My Parents Up", "Manga", "Read", "https://cdn.myanimelist.net/images/manga/1/212825l.jpg");
 addItem("library", "Lonely Girl ni Sakaraenai", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/2/246835l.jpg");
@@ -149,6 +150,7 @@ addItem("library", "My Wish Is to Fall in Love Until You Die.", "Manga", "Unread
 addItem("library", "I Can't Believe I Slept With You!", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/2/226062l.jpg");
 addItem("library", "The Summer You Were There", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/1/239675l.jpg");
 addItem("library", "Kimi wa Natsu no Naka", "Manga", "Unread", "https://cdn.myanimelist.net/images/manga/3/206562l.jpg");
+addItem("library", "Scott Pilgrim", "Graphic Novel", "Unread", "https://upload.wikimedia.org/wikipedia/en/3/39/ScottPilgrim.jpg");
 document.write('</div>');
 
-document.write('<footer>None of the images listed above are mine. Credit to each and every of the official authors.</footer>')
+document.write('<footer>None of the images above are mine. Credit to their respective authors.</footer>');
