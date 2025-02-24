ef7040b55d4eaefc2494994d471624b6907c00b0
kareiku <xaxvu7@gmail.com>
2025-02-24 20:15:51 +0100

    empty commit message

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ function playBackgroundMusic(path) {
 }
 
 function writeRepos(repositories) {
-    const container = document.getElementById("repo-cards-container");
+    const container = document.getElementById("card-container");
     repositories.forEach(repository => {
         const card = document.createElement("div");
         card.className = "card";
+        card.addEventListener("click", () => window.open((repository.homepage || repository.html_url), "_self"));
+        container.appendChild(card);
 
         const title = document.createElement("h3");
         title.textContent = repository.name;
@@ -30,12 +32,5 @@ function writeRepos(repositories) {
         const description = document.createElement("p");
         description.textContent = repository.description || "";
         card.appendChild(description);
-
-        const link = document.createElement("a");
-        link.href = repository.homepage || repository.html_url;
-        link.textContent = "View";
-        card.appendChild(link);
-
-        container.appendChild(card);
     });
 }
