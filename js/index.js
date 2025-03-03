1f27f214a5b033f41d1ab87e6a73f796b93b01ec
kareiku <xaxvu7@gmail.com>
2025-03-03 19:39:01 +0100

    Added the GitHub icons to the navigation bar, linked to my GitHub and Gists pages

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ window.addEventListener("load", () => {
     buildNavbar(["library", "modlists", "piano"]);
 });
 
-function buildNavbar(related) {
+function buildNavbar(pagenames) {
     const navbar = document.getElementById("navigation-bar");
-    related.forEach(element => {
+    pagenames.forEach(element => {
         const btn = document.createElement("input");
         btn.type = "button";
         btn.value = firstCapitalized(element);
@@ -12,6 +12,26 @@ function buildNavbar(related) {
         btn.addEventListener("click", () => window.open(`pages/${element}.html`, "_self"));
         navbar.appendChild(btn);
     });
+
+    const githubBtn = document.createElement("a");
+    githubBtn.href = "https://github.com/kareiku";
+
+    const githubImg = document.createElement("img");
+    githubImg.className = "icon";
+    githubImg.src = "assets/icons/github-mark.svg";
+    githubBtn.appendChild(githubImg);
+
+    navbar.appendChild(githubBtn);
+
+    const gistsBtn = document.createElement("a");
+    gistsBtn.href = "https://gists.github.com/kareiku";
+
+    const gistsImg = document.createElement("img");
+    gistsImg.className = "icon";
+    gistsImg.src = "assets/icons/github-mark-white.svg";
+    gistsBtn.appendChild(gistsImg);
+
+    navbar.appendChild(gistsBtn);
 }
 
 const firstCapitalized = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
