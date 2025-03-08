f806a9a0cfdf4fb48ddf4fc717b1a8cac2e294d3
kareiku <xaxvu7@gmail.com>
2025-03-08 19:45:49 +0100

    chore!: use of classes Button and ImgButton

--- js/index.js
+++ js/index.js
@@ -1,37 +1,11 @@
+import { Button, ImgButton } from './Button.js';
+
 window.addEventListener("load", () => {
-    buildNavbar(["library", "modlists", "piano"]);
-});
-
-function buildNavbar(pagenames) {
     const navbar = document.getElementById("navigation-bar");
-    pagenames.forEach(element => {
-        const btn = document.createElement("input");
-        btn.type = "button";
-        btn.value = firstCapitalized(element);
-        btn.className = "button";
-        btn.addEventListener("click", () => window.open(`pages/${element}.html`, "_self"));
-        navbar.appendChild(btn);
-    });
 
-    const githubBtn = document.createElement("a");
-    githubBtn.href = "https://github.com/kareiku";
-
-    const githubImg = document.createElement("img");
-    githubImg.className = "icon";
-    githubImg.src = "assets/icons/github-mark.svg";
-    githubBtn.appendChild(githubImg);
-
-    navbar.appendChild(githubBtn);
-
-    const gistsBtn = document.createElement("a");
-    gistsBtn.href = "https://gists.github.com/kareiku";
-
-    const gistsImg = document.createElement("img");
-    gistsImg.className = "icon";
-    gistsImg.src = "assets/icons/github-mark-white.svg";
-    gistsBtn.appendChild(gistsImg);
-
-    navbar.appendChild(gistsBtn);
-}
-
-const firstCapitalized = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    navbar.appendChild(new Button("pages/library.html", "Library", "button").create());
+    navbar.appendChild(new Button("pages/modlists.html", "Modlists", "button").create());
+    navbar.appendChild(new Button("pages/piano.html", "Piano", "button").create());
+    navbar.appendChild(new ImgButton("https://github.com/kareiku", "assets/icons/github-mark.svg", "icon").create());
+    navbar.appendChild(new ImgButton("https://gists.github.com/kareiku", "assets/icons/github-mark-white.svg", "icon").create());
+});
