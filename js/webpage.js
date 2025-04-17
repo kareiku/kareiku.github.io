3b95970dcfcce97f8906068779676d6262d663a1
kareiku <xaxvu7@gmail.com>
2025-04-17 04:43:31 +0200

    feat: add script to create a good navbar

---
+++ js/webpage.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () => {
+    document.body.prepend(el('div', { className: 'navbar' }, 
+        el('a', { href: '/index.html' }, el('b', { textContent: 'Home' })),
+        el('a', { href: '/pages/library.html', textContent: 'Library' }),
+        el('a', { href: '/pages/modlists.html', textContent: 'Modlists' }),
+        el('a', { href: '/pages/sqlite.html', textContent: 'SQLite' }),
+        el('a', { href: '/pages/piano.html', textContent: 'Piano' }),
+        el('div', { className: 'separator' }),
+        el('a', { href: 'https://github.com/kareiku', target: '_blank' }, el('img', { src: '/assets/icons/github-mark.svg', alt: 'GitHub' }))
+    ));
+});
+
+function el(tag, attrs = {}, ...children) {
+    const element = document.createElement(tag);
+    Object.entries(attrs).forEach(([key, value]) => element[key] = value);
+    children?.forEach(c => element.appendChild(c));
+    return element;
+}
