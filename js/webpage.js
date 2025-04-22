a268dfd6df6b9749038ef1f533d5230dccde345f
kareiku <xaxvu7@gmail.com>
2025-04-22 20:59:55 +0200

    chore: modify pages

--- js/webpage.js
+++ js/webpage.js
@@ -3,9 +3,8 @@ import el from '/js/functions/el.js';
 window.addEventListener('load', () => {
     document.body.prepend(el('div', { className: 'navbar' }, 
         el('a', { href: '/index.html' }, el('b', { textContent: 'Home' })),
-        el('a', { href: '/pages/library.html', textContent: 'Library' }),
-        el('a', { href: '/pages/modlists.html', textContent: 'Modlists' }),
-        el('a', { href: '/pages/sqlite.html', textContent: 'SQLite' }),
+        el('a', { href: '/pages/sqlite.html', textContent: 'SQLite Viewer' }),
+        el('a', { href: '/pages/piano.html', textContent: 'Piano', target: '_blank' }),
         el('div', { className: 'separator' }),
         el('a', { href: 'https://github.com/kareiku', target: '_blank' }, el('img', { src: '/assets/icons/github.svg', alt: 'GitHub' }))
     ));
